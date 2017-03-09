e00fc8d59caafba3cccc5db5fb6ce8e5f0099629
kuoshuyu <bis2107@hotmail.com>
2017-03-09 11:32:24 +0800

    homework initialized

---
+++ AccountRecord/AccountRecord/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AccountRecord.Startup))]
+namespace AccountRecord
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
