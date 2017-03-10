bf9556f48fb3a4bb8131f46eeb1124c0e3d446ea
kuoshuyu <bis2107@hotmail.com>
2017-03-10 11:52:28 +0800

    UpdateFirst

---
+++ AccountRecord/AccountRecord/Controllers/RecordController.cs
@@ -0,0 +1,29 @@
+﻿using AccountRecord.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AccountRecord.Controllers
+{
+    public class RecordController : Controller
+    {
+        // GET: Record
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public ActionResult DetailList()
+        {
+            var itemlist = new List<RecordViewModel>
+            {
+                new RecordViewModel {Type="收入",TotalAmount=1000,Date=new DateTime(2017,3,9)},
+                new RecordViewModel {Type="支出",TotalAmount=200,Date=new DateTime(2017,5,9)}
+            };
+            return View(itemlist);
+        }
+
+    }
+}
